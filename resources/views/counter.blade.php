81627885d5c9f162a16c0967269178de16256022
jonathan <jonathan.muthiani@squaddigital.com>
2020-07-22 17:04:43 +0300

    added jquery cdn

--- resources/views/counter.blade.php
+++ resources/views/counter.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <title>Pusher Test</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://js.pusher.com/6.0/pusher.min.js"></script>
   <script>
 
